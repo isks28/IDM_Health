bfd49b76e5f4b8e92f6e58f36e0d2e6411f5c260
isks34 <irnu.suryohadi.kusumo@ges.thm.de>
2024-09-03 10:49:14 +0200

    Initial Commit

---
+++ rawDataiOSAppAcquisition/rawDataiOSAppAcquisitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  rawDataiOSAppAcquisitionApp.swift
+//  rawDataiOSAppAcquisition
+//
+//  Created by Irnu Suryohadi Kusumo on 03.09.24.
+//
+
+import SwiftUI
+
+@main
+struct rawDataiOSAppAcquisitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
