6434a4a246d85bdb36e577be543d75653c2f1484
isks34 <irnu.suryohadi.kusumo@ges.thm.de>
2024-10-10 14:14:26 +0200

    2nd push of before

--- rawDataiOSAppAcquisition/rawDataiOSAppAcquisitionApp.swift
+++ rawDataiOSAppAcquisition/rawDataiOSAppAcquisitionApp.swift
@@ -10,9 +10,6 @@ import SwiftUI
 @main
 struct rawDataiOSAppAcquisitionApp: App {
     
-    // Connect AppDelegate to SwiftUI life cycle
-        @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
