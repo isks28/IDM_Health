3cade460d3292f4f04de4fd3086b2e250beaef97
isks34 <irnu.suryohadi.kusumo@ges.thm.de>
2025-02-05 09:57:01 +0100

    removing not working file

--- Pods/Target Support Files/LiteRTSwift/LiteRTSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LiteRTSwift : NSObject
-@end
-@implementation PodsDummy_LiteRTSwift
-@end
--- Pods/Target Support Files/Pods-MotionAnalysis/Pods-MotionAnalysis-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MotionAnalysis : NSObject
-@end
-@implementation PodsDummy_Pods_MotionAnalysis
-@end
