a68b045b706edb6b8c482d4b7dd2155ceca516d3
isks34 <irnu.suryohadi.kusumo@ges.thm.de>
2025-02-11 10:32:39 +0100

    Version 1.0

---
+++ rawDataiOSAppAcquisition/Visual Data/Markerless Motion Analysis/TensorflowMotionAnalysisView.swift
@@ -0,0 +1,19 @@
+//
+//  TensorflowMotionAnalysisView.swift
+//  rawDataiOSAppAcquisition
+//
+//  Created by Irnu Suryohadi Kusumo on 11.02.25.
+//
+
+import SwiftUI
+
+struct TensorflowMotionAnalysisView: View {
+    var body: some View {
+        Text("Work in Progress!")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    TensorflowMotionAnalysisView()
+}
--- rawDataiOSAppAcquisition/Visual Data/visualDataView.swift
+++ rawDataiOSAppAcquisition/Visual Data/visualDataView.swift
@@ -67,7 +67,7 @@ struct visualDataView: View {
                 case "Photo and Video Data":
                     PhotosAndVideoView()
                 case "Markerless Motion Data":
-                    JointView()
+                    TensorflowMotionAnalysisView()
                 default:
                     Text("Unknown View: \(selectedView)")
                 }
